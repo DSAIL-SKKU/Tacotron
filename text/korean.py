ecbca4a66510a12955ec34386108a2916ab2e8c9
yoonjeewoo <yoonjeewoo@gmail.com>
2020-01-22 14:04:48 +0900

    [JW] remove number to korean

--- text/korean.py
+++ text/korean.py
@@ -253,76 +253,76 @@ count_tenth_dict = {
 }
 
 
-def number_to_korean(num_str, is_count=False):
-    if is_count:
-        num_str, unit_str = num_str.group(1), num_str.group(2)
-    else:
-        num_str, unit_str = num_str.group(), ""
-
-    num_str = num_str.replace(',', '')
-    num = ast.literal_eval(num_str)
-
-    if num == 0:
-        return "영"
-
-    check_float = num_str.split('.')
-    if len(check_float) == 2:
-        digit_str, float_str = check_float
-    elif len(check_float) >= 3:
-        raise Exception(" [!] Wrong number format")
-    else:
-        digit_str, float_str = check_float[0], None
-
-    if is_count and float_str is not None:
-        raise Exception(" [!] `is_count` and float number does not fit each other")
-
-    digit = int(digit_str)
-
-    if digit_str.startswith("-"):
-        digit, digit_str = abs(digit), str(abs(digit))
-
-    kor = ""
-    size = len(str(digit))
-    tmp = []
-
-    for i, v in enumerate(digit_str, start=1):
-        v = int(v)
-
-        if v != 0:
-            if is_count:
-                tmp += count_to_kor1[v]
-            else:
-                tmp += num_to_kor1[v]
-
-            tmp += num_to_kor3[(size - i) % 4]
-
-        if (size - i) % 4 == 0 and len(tmp) != 0:
-            kor += "".join(tmp)
-            tmp = []
-            kor += num_to_kor2[int((size - i) / 4)]
-
-    if is_count:
-        if kor.startswith("한") and len(kor) > 1:
-            kor = kor[1:]
-
-        if any(word in kor for word in count_tenth_dict):
-            kor = re.sub(
-                '|'.join(count_tenth_dict.keys()),
-                lambda x: count_tenth_dict[x.group()], kor)
-
-    if not is_count and kor.startswith("일") and len(kor) > 1:
-        kor = kor[1:]
-
-    if float_str is not None:
-        kor += "쩜 "
-        kor += re.sub('\d', lambda x: num_to_kor[x.group()], float_str)
-
-    if num_str.startswith("+"):
-        kor = "플러스 " + kor
-    elif num_str.startswith("-"):
-        kor = "마이너스 " + kor
-
-    return kor + unit_str
+# def number_to_korean(num_str, is_count=False):
+#     if is_count:
+#         num_str, unit_str = num_str.group(1), num_str.group(2)
+#     else:
+#         num_str, unit_str = num_str.group(), ""
+#
+#     num_str = num_str.replace(',', '')
+#     num = ast.literal_eval(num_str)
+#
+#     if num == 0:
+#         return "영"
+#
+#     check_float = num_str.split('.')
+#     if len(check_float) == 2:
+#         digit_str, float_str = check_float
+#     elif len(check_float) >= 3:
+#         raise Exception(" [!] Wrong number format")
+#     else:
+#         digit_str, float_str = check_float[0], None
+#
+#     if is_count and float_str is not None:
+#         raise Exception(" [!] `is_count` and float number does not fit each other")
+#
+#     digit = int(digit_str)
+#
+#     if digit_str.startswith("-"):
+#         digit, digit_str = abs(digit), str(abs(digit))
+#
+#     kor = ""
+#     size = len(str(digit))
+#     tmp = []
+#
+#     for i, v in enumerate(digit_str, start=1):
+#         v = int(v)
+#
+#         if v != 0:
+#             if is_count:
+#                 tmp += count_to_kor1[v]
+#             else:
+#                 tmp += num_to_kor1[v]
+#
+#             tmp += num_to_kor3[(size - i) % 4]
+#
+#         if (size - i) % 4 == 0 and len(tmp) != 0:
+#             kor += "".join(tmp)
+#             tmp = []
+#             kor += num_to_kor2[int((size - i) / 4)]
+#
+#     if is_count:
+#         if kor.startswith("한") and len(kor) > 1:
+#             kor = kor[1:]
+#
+#         if any(word in kor for word in count_tenth_dict):
+#             kor = re.sub(
+#                 '|'.join(count_tenth_dict.keys()),
+#                 lambda x: count_tenth_dict[x.group()], kor)
+#
+#     if not is_count and kor.startswith("일") and len(kor) > 1:
+#         kor = kor[1:]
+#
+#     if float_str is not None:
+#         kor += "쩜 "
+#         kor += re.sub('\d', lambda x: num_to_kor[x.group()], float_str)
+#
+#     if num_str.startswith("+"):
+#         kor = "플러스 " + kor
+#     elif num_str.startswith("-"):
+#         kor = "마이너스 " + kor
+#
+#     return kor + unit_str
 
 
 if __name__ == "__main__":
